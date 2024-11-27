7db87aeb812014683b52a661085b4fd7010221b8
Gabs051 <gabz15000@gmail.com>
2024-11-27 19:40:40 -0300

    sdadasda

--- cadastro/viewOS.php
+++ cadastro/viewOS.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php 
     require_once($_SERVER['DOCUMENT_ROOT'] . '/bytecare/templates/header.php');
     require_once __DIR__ . '/../../vendor/tecnickcom/tcpdf/tcpdf.php';
